a17f869ac166722347f7dbd6b8614ee67cf0c4ea
koljapluemer <github@koljapluemer.com>
2025-06-02 15:32:52 +0200

    simplify model

--- entities/models/exercise/exercise_flashcard.py
+++
@@ -1,6 +0,0 @@
-from entities.models.exercise.exercise import Exercise
-
-
-class ExerciseFlashcard(Exercise):
-    def __str__(self):
-        return self.extra_content_before_solve + " | " + self.extra_content_after_solve
