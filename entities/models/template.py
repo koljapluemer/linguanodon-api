55305dc2792af7aa8ccb0bc98271a7343957e951
koljapluemer <github@koljapluemer.com>
2025-06-02 14:13:06 +0200

    add lang goal relation to template

--- entities/models/template.py
+++ entities/models/template.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from entities.models.language import Language
-
+from entities.models.learning_goal import LearningGoal
 class Template(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -9,6 +9,7 @@ class Template(models.Model):
     is_public = models.BooleanField(default=False)
     is_approved = models.BooleanField(default=False)
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    learning_goals = models.ManyToManyField(LearningGoal)
 
     derived_from = models.ForeignKey("self", on_delete=models.CASCADE, blank=True, null=True)
 
