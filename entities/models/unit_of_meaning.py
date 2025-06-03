0bb94900b9b7ab8d7fdb559a1513333f45d11939
koljapluemer <github@koljapluemer.com>
2025-06-03 14:31:03 +0200

    adapt 100 common words script to new model structure

--- entities/models/unit_of_meaning.py
+++ entities/models/unit_of_meaning.py
@@ -3,6 +3,7 @@ from django.db import models
 from entities.models.learning_goal import LearningGoal
 
 class UnitOfMeaning(models.Model):
+    based_on_meaning = models.TextField(unique=True)
     image = models.ImageField(upload_to='unit_of_meaning_images/', blank=True, null=True)
     learning_goals = models.ManyToManyField(LearningGoal, blank=True)
 
