7849b00ba546639258b7bef3fdff0abd6349504f
koljapluemer <github@koljapluemer.com>
2025-06-04 16:13:03 +0200

    add red test for test_list_translations_in_language_for_unit_of_meaning

---
+++ api/tests/test_list_translations_in_language_for_unit_of_meaning.py
@@ -0,0 +1,32 @@
+import json
+import os
+from django.test import TestCase
+from django.urls import reverse
+from django.core.management import call_command
+
+class TestListTranslationsInLanguageForUnitOfMeaning(TestCase):
+    def setUp(self):
+        # Load fixture using absolute path
+        fixture_path = os.path.join(os.path.dirname(__file__), 'test_list_translations_in_language_for_unit_of_meaning_fixture.json')
+        call_command('loaddata', fixture_path)
+            
+        # Load expected response
+        expected_response_path = os.path.join(os.path.dirname(__file__), 'test_list_translations_in_language_for_unit_of_meaning_expected_response.json')
+        with open(expected_response_path) as f:
+            self.expected_response = json.load(f)
+
+    def test_list_translations_in_language_for_unit_of_meaning(self):
+        # Given
+        unit_of_meaning_id = 1
+        language_code = 'en'
+        url = reverse('list_translations_in_language_for_unit_of_meaning', kwargs={
+            'unit_of_meaning_id': unit_of_meaning_id,
+            'language_code': language_code
+        })
+
+        # When
+        response = self.client.get(url)
+
+        # Then
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content), self.expected_response)
