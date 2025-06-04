3b80e4d35ad0d0ce0d04e9ad82a68d875be0d61b
koljapluemer <github@koljapluemer.com>
2025-06-04 10:30:53 +0200

    delete old api app

--- api/enpoints/learning_goals/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from entities.models.learning_goal import LearningGoal
-
-class LearningGoalSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = LearningGoal
-        fields = ['id', 'name'] 
