69963f618620c6751e874ec1c1b38e2be4242aa9
koljapluemer <github@koljapluemer.com>
2025-06-09 11:12:22 +0200

    pack up old attempts

--- linguanodon/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class LinguanodonConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'linguanodon'
-    
-    def ready(self):
-        import linguanodon.models
